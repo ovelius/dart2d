ecc7ba90f927a584fdbd14a028ec13b318127162
ovelius <erik.ovelius@gmail.com>
2015-03-29 18:12:21 +0200

    Move space specific logic to space_world.dart

--- lib/worlds/world.dart
+++ lib/worlds/world.dart
@@ -122,6 +122,8 @@ abstract class World {
     canvas.restore();
   }
   
+  void collisionCheck(int networkId, duration);
+  
   void putPendingSpritesInWorld() {
     while (waitingSprites.length > 0) {
       Sprite newSprite = waitingSprites.removeAt(0);
@@ -152,15 +154,11 @@ abstract class World {
     }
     serverFrame += frames;
     serverFps.timeWithFrames(duration, frames);
-    if (network.isServer()
-      //  && network.hasReadyConnection() This is commented out to allow local testing.
-        && sprites.length < 40
-        && frames > 0
-        && new Random().nextDouble() > 0.99) {
-      addAstroid(200.0);
-    }
+    think(duration, frames);
     return frames;
   }
+  
+  void think(double duration, int frames);
 
   MovingSprite getOrCreateSprite(int networkId, int flags, ConnectionWrapper wrapper) {
     Sprite sprite = sprites[networkId];
@@ -191,29 +189,6 @@ abstract class World {
             this, localKeyState, 400.0, 200.0, spriteIndex));
   }
 
-  void collisionCheck(int networkId, duration) {
-    if (network.isServer()) {
-      var sprite = sprites[networkId];
-      if (sprite is MovingSprite) {
-        if (!sprite.collision) return;
-        for (int id in sprites.keys) {
-          // Avoid duplicate checks.
-          if (networkId >= id) {
-            continue;
-          }
-          var otherSprite = sprites[id];
-          if (otherSprite is MovingSprite) {
-            if (!otherSprite.collision) continue;
-            if (collision(sprite, otherSprite, duration)) {
-              sprite.collide(otherSprite);
-              otherSprite.collide(sprite);
-            }
-          }
-        }   
-      }
-    }
-  }
-  
   addLocalPlayerSprite(String name) {
     Server server = network as Server;
     int id = server.gameState.getNextUsablePlayerSpriteId();
@@ -273,25 +248,7 @@ abstract class World {
   
   num negate(Random r) {
     return r.nextBool() ? 1 : -1;    
-  }
-
-  void addAstroid(double size, [Vec2 pos]) {
-    Random r = new Random();
-    var sprite = new Astroid(this,
-        size.toInt(), 0.0, 0.0, imageByName['astroid.png']);
-    if (pos == null) {
-      sprite.position = sprite.position - sprite.size;
-    } else {
-      sprite.position = pos;
-    }
-    sprite.velocity = new Vec2.random(negate(r) * 20, negate(r) * 20);
-    sprite.rotationVelocity = negate(r) * new Random().nextDouble() * 2.0;
-    sprite.size = new Vec2(size, size);
-    sprite.setRadius(0.8 * (sprite.size.sum() / 2));
-    sprite.networkType = NetworkType.LOCAL;
-    addSprite(sprite);
-  }
-  
+  }  
   toString() => "World[${peer.id}]";
 }
 
