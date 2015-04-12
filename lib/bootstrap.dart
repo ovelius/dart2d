b800b64237662866dd15fd77cffeb7bb2c295ee9
ovelius <erik.ovelius@gmail.com>
2015-04-12 20:49:48 +0200

    Don't allow the world to frameskip, instead slow down the game.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -7,13 +7,12 @@ import 'package:dart2d/sprites/movingsprite.dart';
 import 'package:dart2d/phys/phys.dart';
 import 'package:dart2d/net/rtc.dart';
 import 'package:dart2d/phys/vec2.dart';
-import 'package:dart2d/worlds/space_world.dart';
 import 'package:dart2d/worlds/world.dart';
 import 'dart:html';
 import 'dart:math';
 import 'dart:async';
 
-const TIMEOUT = const Duration(milliseconds: 21);
+const Duration TIMEOUT = const Duration(milliseconds: 21);
 DateTime lastStep;
 
 World world;
@@ -34,10 +33,16 @@ void step() {
   int millis = now.millisecondsSinceEpoch - lastStep.millisecondsSinceEpoch;
   assert(millis >= 0);
   double secs = millis / 1000.0;
-  assert(secs >= 0.0);
+  if (secs >= 0.041) {
+    // Slow down the game instead of skipping frames.
+    secs = 0.041;
+  }
   world.frameDraw(secs);
   lastStep = now;
   
   int frameTimeMillis = new DateTime.now().millisecondsSinceEpoch - startStep.millisecondsSinceEpoch;
+  if (frameTimeMillis > TIMEOUT.inMilliseconds) {
+    
+  }
   new Timer(TIMEOUT - new Duration(milliseconds: frameTimeMillis), step);
 }
