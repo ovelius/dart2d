005280ea3e9a7a30ea114ab9da13b222ed50d8a3
ovelius <erik.ovelius@gmail.com>
2015-10-11 01:14:14 +0200

    Add basic google play games sign in

--- web/wormworld.dart
+++ web/wormworld.dart
@@ -4,12 +4,21 @@ import 'package:dart2d/bootstrap.dart';
 import 'package:dart2d/worlds/worm_world.dart';
 import 'package:dart2d/worlds/world.dart';
 import 'package:dart2d/res/imageindex.dart';
+import 'dart:js';
 import 'dart:html';
 
 final int WIDTH = (querySelector("#canvas") as CanvasElement).width;
 final int HEIGHT = (querySelector("#canvas") as CanvasElement).height;
 
 void main() {
+  context['onSignIn'] = (param) {
+     JsObject user = param;
+     JsObject profile = user.callMethod('getBasicProfile');
+     String name = profile.callMethod('getName');
+     (querySelector("#nameInput") as InputElement).value = name;
+     world.playerName = name;
+  };
+  
   canvasElement = (querySelector("#canvas") as CanvasElement);
   canvas = canvasElement.context2D;
   world = new WormWorld(WIDTH, HEIGHT);
@@ -22,7 +31,7 @@ void main() {
     var clientId = (querySelector("#clientId") as InputElement).value;
     var name = (querySelector("#nameInput") as InputElement).value;
     world.restart = true;
-    world.connectTo(clientId, name);
+    world.connectTo(clientId);
   });
   
   querySelector("#sendMsg").onClick.listen((e) {
