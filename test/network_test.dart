5a26251a2fe7c480eccd7098af3a794acbdbe4b1
ovelius <erik.ovelius@gmail.com>
2017-01-09 13:57:50 +0100

    Update network test

--- test/network_test.dart
+++ test/network_test.dart
@@ -19,6 +19,8 @@ void main() {
   MockKeyState mockKeyState;
   TestPeer peer;
   Network network;
+  TestConnection connectionB;
+  TestConnection connectionC;
   setUp(() {
     mockHudMessages = new MockHudMessages();
     mockSpriteIndex = new MockSpriteIndex();
@@ -31,31 +33,45 @@ void main() {
     when(mockSpriteIndex.spriteIds()).thenReturn(new List());
     when(mockKeyState.getEnabledState()).thenReturn(FAKE_ENABLED_KEYS);
     remapKeyNamesForTest();
-  });
-  test('Test basic client network update', () {
-    network.frame(0.01, new List());
-
-    TestConnection connectionB = new TestConnection('b');
-    TestConnection connectionC = new TestConnection('c');
+    connectionB = new TestConnection('b');
+    connectionC = new TestConnection('c');
     connectionB.setOtherEnd(connectionC);
     connectionC.setOtherEnd(connectionB);
     connectionC.bindOnHandler('data', (unused, data) {
       print("Got data ${data}");
     });
+  });
+
+  test('Test basic client network update', () {
+    network.frame(0.01, new List());
+
     network.peer.connectPeer(null, connectionB);
 
     network.frame(0.01, new List());
     expect(connectionC.decodedRecentDataRecevied(),
         equals({KEY_STATE_KEY:FAKE_ENABLED_KEYS, KEY_FRAME_KEY: 0}));
 
+    _TestSprite sprite = new _TestSprite.withVecPosition(1000, new Vec2(9, 9));
     when(mockSpriteIndex.spriteIds()).thenReturn(new List.filled(1, 1000));
-    when(mockSpriteIndex[1000]).thenReturn(new _TestSprite.withVecPosition(1000, new Vec2(9, 9)));
+    when(mockSpriteIndex[1000]).thenReturn(sprite);
 
     network.frame(0.01, new List());
 
     // Full state sent over network.
     expect(connectionC.decodedRecentDataRecevied()['1000'],
-        equals([SpriteConstructor.DAMAGE_PROJECTILE.index,99,9,9,0,180000,180000,1,null,2,2,1,0]));
+        equals([SpriteConstructor.DAMAGE_PROJECTILE.index,101,9,9,0,180000,180000,1,null,2,2,1,0]));
+
+    // Sprites only send full state of a while.
+    while (sprite.fullFramesOverNetwork > 0) {
+      network.frame(0.01, new List());
+    }
+
+    // Now only delta updates.
+    // TODO: Reduce this to only send position/velocity?
+    network.frame(0.01, new List());
+    expect(connectionC.decodedRecentDataRecevied()['1000'],
+        equals([SpriteConstructor.DAMAGE_PROJECTILE.index, 101, 9, 9, 0, 180000, 180000]));
+
   });
 }
 
@@ -79,7 +95,7 @@ class _TestSprite extends MovingSprite {
   }
 
   int sendFlags() {
-    return 99;
+    return 101;
   }
 
   SpriteConstructor remoteRepresentation() {
