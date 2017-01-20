d002ecb29a761381de3ae839ab511550365a6fbd
ovelius <erik.ovelius@gmail.com>
2017-01-20 14:36:11 +0100

    Create injector using modules

--- lib/net/net.dart
+++ lib/net/net.dart
@@ -1,7 +1,17 @@
 library net;
 
+import 'package:di/di.dart';
+import "net.dart";
+
 export 'network.dart';
 export 'peer.dart';
 export 'connection.dart';
 export 'state_updates.dart';
 export 'chunk_helper.dart';
+
+class NetModule extends Module {
+ NetModule() {
+   bind(Network);
+   bind(ChunkHelper);
+ }
+}
--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,7 +1,19 @@
 library util;
 
+import 'package:di/di.dart';
+import "util.dart";
+
 export 'fps_counter.dart';
 export 'hud_messages.dart';
 export 'keystate.dart';
 export 'mobile_controls.dart';
 export 'gamestate.dart';
+
+class UtilModule extends Module {
+  UtilModule() {
+    bind(GameState);
+    bind(FpsCounter);
+    bind(HudMessages);
+    bind(MobileControls);
+  }
+}
